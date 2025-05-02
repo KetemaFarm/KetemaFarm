f9c2b32a9ee0acffc115842907bb66243d207567
Yarbework <yarbemohaz3517@gmail.com>
2025-05-03 02:09:23 +0300

    courses page main components are added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const categories = [
+  "show All",
   "General Farming Tutorials",
   "planting & Growing",
   "Maintenance & Care",
@@ -8,17 +9,19 @@ const categories = [
 ];
 function Sidebar({ selectedCategory, onSelectCategory }) {
   return (
-    <aside className="w-64 bg-white p-4 shadow-sm flex-shrink-0 mt-30">
-      <h2 className="text-sm font-semibold text-gray-500 uppwecase mb-4">
-        Catagories
+    <aside className="w-64 bg-white p-4 shadow-sm flex-shrink-0 mt-10 md:mt-16 lg:mt-20">
+      <h2 className="text-lg font-semibold text-gray-700 uppercase mb-4 tracking-wide">
+        Categories
       </h2>
       <ul>
         {categories.map((category) => (
           <li key={category} className="mb-2">
             <button
               onClick={() => onSelectCategory(category)}
-              className={`w-full text-left px-3 py-1 rounded text-gray-700 hover:bg-gray-100 ${
-                selectedCategory === category ? "font-semibold bg-gray-100" : ""
+              className={`w-full text-left px-3 py-2 rounded text-gray-600 hover:bg-gray-100 hover:text-green-800 tranisition-colors duration-150 ${
+                selectedCategory === category
+                  ? "font-semibold bg-greeen-100 text-green-800"
+                  : "font-medium"
               }`}
             >
               {category}
--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,14 +1,17 @@
 //main link: https://www.googleapis.com/youtube/v3
-//API Key: AIzaSyD-WL6Uh7BYFvaLt9DKfXyLi3bd0oLZs64
-//full request: https://www.googleapis.com/youtube/v3/search?key={your-api-key}&channelId={channel-id-here}&part=snippet,id&order=date&maxResults={number}
+//full request: https://www.googleapis.com/youtube/v3/search?key=AIzaSyD-WL6Uh7BYFvaLt9DKfXyLi3bd0oLZs64&channelId=UC_PgChfO-fgSIpIYWD3Ka-g&part=snippet,id&order=date&maxResults=15
 
-import Sidebar from "../components/Sidebar";
+// channelID 1: UC_PgChfO-fgSIpIYWD3Ka-g
+// channelID 2: UCB1J6siDdmhwah7q0O2WJBg
+// channelID 3: UCSbyncU597LMwb3HhnAI_4w
+// channelID 4: UCTZN3HhejW1tOiRdLGUCGGA
 
+import VideosPage from "../components/VideosPage";
 const Courses = () => {
   return (
     <>
       <div>
-        <Sidebar />
+        <VideosPage />
       </div>
     </>
   );
