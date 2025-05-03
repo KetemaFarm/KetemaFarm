41c203fd072f50f74fa7d231397b721f4c2c347b
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-05-03 15:31:38 +0300

    finished styling the merged products

--- frontend/src/components/FeaturedProducts.jsx
+++ frontend/src/components/FeaturedProducts.jsx
@@ -5,15 +5,17 @@ const FeaturedProducts = () => {
   return (
     <section className="py-24">
       <div>
-        <h1 className="text-2xl font-bold">Featured Marketplace Products</h1>
+        <h1 className="text-2xl font-bold font-['Montserrat'] text-green-900 text-center">
+          Featured Marketplace Products
+        </h1>
         <ProductsGrid />
       </div>
       <div className="py-8 mx-auto flex items-center">
         <Link
           to="products"
-          className="capitalize px-4 py-2 bg-green-600 text-white inline-block mx-auto rounded-lg"
+          className="capitalize px-4 py-2  text-green-900 font-['Kanit'] border-1 border-green-950 hover:bg-green-900 hover:text-gray-100 inline-block mx-auto rounded-lg"
         >
-          View all produce
+          View all product
         </Link>
       </div>
     </section>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 import hero from "../assets/heroImg.jpg";
 const Hero = () => {
   return (
-    <div className="flex flex-col lg:flex-row lg:justify-between gap-3 items-center">
-      <div className="mx-3 flex flex-col items-center ">
-        <p className="text-4xl font-bold text-center tracking-tight  sm:text-5xl lg:text-6xl font-['Montserrat'] lg:text-start lg:w-120">
+    <div className="flex flex-col lg:flex-row lg:justify-between gap-3 justify-start items-center">
+      <div className="mx-3 flex flex-col items-center lg:items-start  lg:ml-10 ">
+        <p className="text-4xl font-bold text-center tracking-tight sm:text-5xl lg:text-6xl font-['Montserrat']  lg:text-start ">
           Your Urban Farming Marketplace Grow, Sell, Thrive!
         </p>
 
-        <p className="mt-8 leading-6 font-['Montserrat'] text-sm  text-center lg:text-start lg:w-80">
+        <p className="mt-8 leading-6 font-['Montserrat'] text-sm  text-center lg:text-start  ">
           Buy fresh farm products, rent urban land, or sell your harvest – all
           in one place." "Join as a buyer, landowner, or seller to grow your
           city’s green future.
         </p>
-        <div className="mt-10   flex flex-row gap-4 justify-center">
+        <div className="mt-10   flex flex-row gap-4 justify-center lg:self-start">
           <Link
             to="products"
             className="btn btn-primary font-['Kanit'] bg-green-900 border-1 border-green-900"
@@ -28,7 +28,7 @@ const Hero = () => {
           </Link>
         </div>
       </div>
-      <img src={hero} alt="hero Image" className="w-200 lg:w-150" />
+      <img src={hero} alt="hero Image" className="w-200 lg:w-140" />
     </div>
   );
 };
