bf2096d928d201bdeb4fea467b66d8e282cb8b79
Demtse Yibabe <demtse.yibabe@gmail.com>
2025-05-03 10:10:58 +0300

    working on the Navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,8 +5,8 @@ import Search from "./Search";
 const Header = () => {
   return (
     <header className="bg-gray-100 shadow-xl text-neutral-content">
-      <div className="w-full  flex flex-row p-1 font-['Kanit']  justify-center  bg-green-700">
-        <div className="flex gap-x-6 justify-center items-center text-black">
+      <div className="w-full  flex flex-row p-1 font-['Kanit']  justify-center  bg-green-700 mb-4">
+        <div className="flex flex-row gap-x-6 justify-center items-center text-black">
           <Link
             to="/login"
             className="link link-hover text-xs sm:text-sm text-gray-300 hover:text-gray-950 underline transition delay-10"
@@ -21,15 +21,24 @@ const Header = () => {
           </Link>
         </div>
       </div>
-      <div className="px-8 flex-col flex justify-between items-center">
-        <img src={logo} alt="logo" />
-        <Search />
-        <div className="flex items-center">
-          <Link to="login" className="bg-green-600 py-2 px-4 rounded-sm">
+      <div className="px-8 flex-col gap-3 flex  justify-between items-center sm:flex-row sm:justify-between">
+        <div className="flex flex-col sm:flex-row sm:justify-between items-center gap-3">
+          <img src={logo} alt="logo" />
+          <Search />
+        </div>
+
+        <div className="flex flex-row items-center">
+          <Link
+            to="login"
+            className="bg-green-700 w-20 h-8 flex justify-center items-center font-['Rubik'] text-[10px] sm:text-xs sm:w-30  rounded-sm"
+          >
             Sell Produce
           </Link>
-          <span className="text-3xl px-4 text-green-600">/</span>
-          <Link to="login" className="bg-green-600 py-2 px-4 rounded-sm">
+          <span className="text-3xl px-4 font-['Rubik'] text-green-700">/</span>
+          <Link
+            to="login"
+            className="bg-green-700 w-20 h-8 flex justify-center items-center font-['Rubik'] text-[10px] sm:text-xs sm:w-30  rounded-sm"
+          >
             Post Land
           </Link>
         </div>
--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,16 +1,18 @@
 import { CiSearch } from "react-icons/ci";
 
-
 const Search = () => {
   return (
-    <form className="relative">
+    <form className="relative flex flex-row">
       <CiSearch className="absolute text-xl top-3 left-1" />
       <input
         type="text"
-        className="py-1.5 px-8 focus:outline-none border-2"
+        className=" w-50 h-8 rounded-l-md focus:outline-none border-2 border-gray-300 font-['Kanit'] text-xs pl-2 text-gray-400"
         placeholder="Search"
       />
-      <button type="submit" className="bg-green-600 py-2 px-4 border-2">
+      <button
+        type="submit"
+        className="bg-green-700  font-['Rubik'] h-8 flex flex-row justify-center items-center text-xs p-1 w-20 rounded-r-md "
+      >
         Search
       </button>
     </form>
